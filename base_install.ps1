b565732b5246ce830584cbbddaa8d8aff17e6147
Fredrik <flarsso93@gmail.com>
2020-02-20 20:19:51 +0100

    Add procmon to base install

--- base_install.ps1
+++ base_install.ps1
@@ -7,7 +7,7 @@ Write-Host "Installing user components" -ForegroundColor Yellow
 & choco install firefox 7zip keepass vlc firacode slack discord everything autohotkey --yes --limit-output
 
 Write-Host "Installing development components" -ForegroundColor Yellow
-& choco install python3 python2 dotnetcore dotnetfx git  --yes --limit-output
+& choco install python3 python2 dotnetcore dotnetfx git procmon --yes --limit-output
 
 Write-Host "Installing editors and IDEs components" -ForegroundColor Yellow
 & choco install neovim visualstudio vscode winmerge notepadplusplus.install --yes --limit-output
