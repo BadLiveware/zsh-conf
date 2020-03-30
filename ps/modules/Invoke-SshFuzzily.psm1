cc62667be7323c1eaa76bbb76e8c718e3b68909b
Fredrik <flarsso93@gmail.com>
2020-03-31 00:14:08 +0200

    Update

--- ps/modules/Invoke-SshFuzzily.psm1
+++ ps/modules/Invoke-SshFuzzily.psm1
@@ -1,4 +1,9 @@
 function Invoke-SshFuzzily {
+    param (
+        [parameter(position=0)]
+        [string]
+        $User = $env:UserName
+    )
     ((Test-CommandExists -Command fzf) || Write-Error "Unable to find required command: fzf\nInstall using `"choco install fzf`""  -ErrorAction Stop) | Out-Null
     ((Test-CommandExists -Command ssh) || Write-Error "Unable to find required command: ssh" -ErrorAction Stop) | Out-Null
 
@@ -6,12 +11,13 @@ function Invoke-SshFuzzily {
     if ($AllHosts.Count -eq 0) {
         Write-Error "No hosts found" -ErrorAction Stop
     }    
-    [string]$SelectedHost = Select-Host -Hosts $AllHosts
+    [string]$SelectedHost = Select-Host -Hosts $AllHosts -User $User
     if ([string]::IsNullOrEmpty($SelectedHost)) {
         Write-Error "No host selected" -ErrorAction Stop
     }
-    Write-Host "Selected host:" $SelectedHost
-    ssh $SelectedHost     
+    Write-Host "Connecting to: " -ForegroundColor Yellow -NoNewline
+    Write-HOst $User@$SelectedHost -ForegroundColor Cyan
+    ssh $User@$SelectedHost
 }
 
 function Get-Hosts {
@@ -23,10 +29,12 @@ function Get-Hosts {
 function Select-Host {
     param (
         [string[]]
-        $Hosts
+        $Hosts,
+        [string]
+        $User
     )
     $Hosts
-    | fzf --height 10 --header "Select the target for ssh" --prompt "> ssh "
+    | fzf --height 10 --header "Select the target for ssh" --prompt "> ssh -l $User "
     | Foreach-Object { $_.split(" ")[0] }
 }
 
