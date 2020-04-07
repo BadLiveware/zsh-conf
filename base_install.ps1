235f8234b75ddcb0c99b69dbd5267538d3f84426
Fredrik Larsson <Fredrik.Larsson@afaforsakring.se>
2020-04-07 13:24:31 +0200

    Update

--- base_install.ps1
+++ base_install.ps1
@@ -1,7 +1,8 @@
 #Requires -RunAsAdministrator
 
 Write-Host "Installing basic shell components" -ForegroundColor Yellow
-& choco install powershell-core starship lf fzf ripgrep curl gsudo bat --yes --limit-output
+& choco install powershell-core starship lf fzf ripgrep curl bat --yes --limit-output
+& choco install gsudo --prerelease --yes --limit-output
 
 Write-Host "Installing user components" -ForegroundColor Yellow
 & choco install firefox 7zip keepass vlc firacode slack discord everything autohotkey powertoys --yes --limit-output
@@ -12,10 +13,10 @@ Write-Host "Installing development components" -ForegroundColor Yellow
 Write-Host "Installing editors and IDEs components" -ForegroundColor Yellow
 & choco install neovim visualstudio vscode winmerge notepadplusplus --yes --limit-output
 
-trite-Host "Installing python components" -ForegroundColor Green
+Write-Host "Installing python components" -ForegroundColor Green
 & python -m pip install --upgrade pip
 & pip install glances
 
 Write-Host "Installing powershell modules"
-$PowershellModules = "PSEverything", "oh-my-posh", "posh-git", "PSFzf"; # PSReadLine is built-in for powershell-core 6
+$PowershellModules = "PSEverything", "PSFzf"; # PSReadLine is built-in for powershell-core 6
 $PowershellModules | ForEach-Object { & pwsh.exe -NonInteractive -NoProfile -NoLogo -Command Install-Module -Name $_ -AllowPrerelease }
