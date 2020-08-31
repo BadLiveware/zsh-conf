e93bcb91c4d63f0fb2993f683602268f67f7baca
Fredrik Larsson <Fredrik.Larsson@afaforsakring.se>
2020-08-31 15:28:57 +0200

    Update

--- starship/set-env.ps1
+++ starship/set-env.ps1
@@ -1,4 +1,4 @@
 Write-Host "Setting STARSHIP_CONFIG to"
-$PSScriptRoot | Tee-Object -Variable Value | Write-Host 
+"$PSScriptRoot/starship.toml" | Tee-Object -Variable Value | Write-Host 
 
 [System.Environment]::SetEnvironmentVariable("STARSHIP_CONFIG", $Value, "User")
