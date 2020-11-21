0bd36ee4af3dfe9fac9e60678302556ca3b7cda7
Fredrik <flarsson93@gmail.com>
2020-11-21 18:30:52 +0100

    Update

--- misc/link.ps1
+++ misc/link.ps1
@@ -1,5 +1,13 @@
 #Requires -RunAsAdministrator
 
-$Target = Join-Path $PSScriptRoot .\Guid-Clipboard.ahk
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "Guid-Clipboard AHK Script" /t REG_SZ /F /D "$Target"
+$Startup = "$([Environment]::GetFolderPath('Startup'))"
+
+$GuidClipboard = Join-Path $PSScriptRoot .\Guid-Clipboard.ahk
+New-Item -ItemType SymbolicLink -Path $Startup -Name "GuidClipboard" -Value $GuidClipboard -Force
+#REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "Guid-Clipboard AHK Script" /t REG_SZ /F /D "$Target"
+
+
+$AlwaysOnTop = Join-Path $PSScriptRoot .\AlwaysOnTop.ahk
+New-Item -ItemType SymbolicLink -Path $Startup -Name "AlwaysOnTop" -Value $AlwaysOnTop -Force
+# REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "AlwaysOnTop AHK Script" /t REG_SZ /F /D "$Target"
 
