1baf1082d6ea30710fc941d15b566088420369f1
Fredrik Larsson <fredrik.larsson@afaforsakring.se>
2022-04-19 09:05:46 +0200

    Remove docker workaround

--- zprofile.zsh
+++ zprofile.zsh
@@ -16,3 +16,20 @@ export LC_TELEPHONE="sv_SE.UTF-8"
 export LC_MEASUREMENT="sv_SE.utf8"
 export LC_IDENTIFICATION="sv_SE.UTF-8"
 export LC_ALL=
+
+# DOCKER_DISTRO="ubuntu"
+# DOCKER_DIR=/mnt/wsl/shared-docker
+# DOCKER_SOCK="$DOCKER_DIR/docker.sock"
+# export DOCKER_HOST="unix://$DOCKER_SOCK"
+# # if [ ! -S "$DOCKER_SOCK" ]; then
+# #    mkdir -pm o=,ug=rwx "$DOCKER_DIR"
+# #    chgrp docker "$DOCKER_DIR"
+# #    /mnt/c/Windows/System32/wsl.exe -d $DOCKER_DISTRO sh -c "nohup sudo -b dockerd < /dev/null > $DOCKER_DIR/dockerd.log 2>&1"
+# # fi
+# if service docker status 2>&1 | grep -q "is not running"; then
+#   mkdir -pm o=,ug=rwx "$DOCKER_DIR"
+#   chgrp docker "$DOCKER_DIR"
+#   wsl.exe -d "${WSL_DISTRO_NAME}" -u root -e /usr/sbin/service docker start >$DOCKER_DIR/dockerd.log 2>&1
+# fi
+
+export DOCKER_BUILDKIT=1
